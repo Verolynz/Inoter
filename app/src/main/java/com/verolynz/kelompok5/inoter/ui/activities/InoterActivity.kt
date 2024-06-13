4a6c5ccd2a73cc4712e42d86407145b1c2b1021e
Verolynz <220441100156@student.trunojoyo.ac.id>
2024-06-13 16:35:27 +0700

    Penambahan notes untuk TODO fungsi cek internet dan kondisi load data.

--- app/src/main/java/com/verolynz/kelompok5/inoter/ui/activities/InoterActivity.kt
+++ app/src/main/java/com/verolynz/kelompok5/inoter/ui/activities/InoterActivity.kt
@@ -3,6 +3,7 @@ package com.verolynz.kelompok5.inoter.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.verolynz.kelompok5.inoter.R
+//Untuk mencapai ini, Anda perlu membuat fungsi yang memeriksa koneksi internet dan berdasarkan status koneksi, memutuskan dari mana data harus dimuat. Anda juga perlu membuat fungsi yang memperbarui database setiap kali aplikasi terhubung ke internet
 
 class InoterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
