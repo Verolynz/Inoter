bdf40cbeecfcb14c324b948253010897e66293fb
Verolynz <220441100156@student.trunojoyo.ac.id>
2024-06-13 17:52:41 +0700

    Penambahan notes untuk TODO fungsi cek internet dan kondisi load data.

--- app/src/main/java/com/verolynz/kelompok5/inoter/utils/DependencyInjectionUtils.kt
+++ app/src/main/java/com/verolynz/kelompok5/inoter/utils/DependencyInjectionUtils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.verolynz.kelompok5.inoter.data.local.OlahragaDB
 import com.verolynz.kelompok5.inoter.data.repositories.OlahragaRepository
 //apa yang dilakukan oleh DependencyInjectionUtils adalah memberikan repository yang dibutuhkan oleh ViewModels
+//hola amigass
 
 
 //apa yang dilakukan oleh DependencyInjectionUtils adalah memberikan repository yang dibutuhkan oleh ViewModels
