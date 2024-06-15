57238714c1bf7699061cdd434ef2f415b8d79fbd
Vigocandra <abimanyunirwana123@gmail.com>
2024-06-15 22:07:57 +0700

    Login

---
+++ app/src/main/java/com/verolynz/kelompok5/inoter/Login.kt
@@ -0,0 +1,18 @@
+package com.verolynz.kelompok5.inoter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
