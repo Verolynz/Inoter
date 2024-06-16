4cebd954b83368c9f592e3d0c2fcf8faa2fa015f
Verolynz <220441100156@student.trunojoyo.ac.id>
2024-06-16 14:07:22 +0700

    pepek

--- app/src/main/java/com/verolynz/kelompok5/inoter/data/local/UsersDao.kt
+++ app/src/main/java/com/verolynz/kelompok5/inoter/data/local/UsersDao.kt
@@ -1,12 +1,14 @@
 package com.verolynz.kelompok5.inoter.data.local
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 
+@Dao
 interface UsersDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
