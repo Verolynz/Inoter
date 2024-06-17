3dd32e43b6853d4e8874a7b9fae6a4b4baa3d9aa
martinhara-trunojoyo <martinharahap02@gmail.com>
2024-06-17 15:58:31 +0700

    add fitur artikel Create Read -Martin

---
+++ app/src/main/java/com/verolynz/kelompok5/inoter/room/AppRepository.kt
@@ -0,0 +1,33 @@
+package com.verolynz.kelompok5.inoter.room
+
+import androidx.lifecycle.LiveData
+import com.bumptech.glide.util.Executors
+import com.verolynz.kelompok5.inoter.utils.ExecutorsUtils
+
+class AppRepository private constructor(private val appDao: AppDao, private val ExecutorsUtils: ExecutorsUtils) {
+
+    // Mendapatkan semua data pemain dari database
+    fun getAllArtikel(): LiveData<List<ArtikelDatabase>> = appDao.getAllArtikel()
+
+    // Memasukkan data pemain ke dalam database
+    fun insertArtikel(artikel: ArtikelDatabase) {
+        // Menjalankan operasi insert di thread yang berbeda
+        ExecutorsUtils.diskIO().execute { appDao.insertArtikel(artikel) }
+    }
+
+    companion object {
+        // Variabel untuk menyimpan instance dari AppRepository
+        @Volatile
+        private var instance: AppRepository? = null
+
+        // Mendapatkan instance dari AppRepository
+        fun getInstance(
+            appDao: AppDao,
+            appExecutors: ExecutorsUtils
+        ): AppRepository =
+            // Jika instance null, maka akan dibuat instance baru
+            instance ?: synchronized(this) {
+                instance ?: AppRepository(appDao, appExecutors)
+            }.also { instance = it }
+    }
+}
